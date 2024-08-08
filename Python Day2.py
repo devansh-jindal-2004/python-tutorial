55770fb4cbf726e5d8a1867525d205a0089a4da6
Devansh <devanshjindal31@gmail.com>
2024-08-08 22:42:17 +0530

    2nd day target un reached

---
+++ Python Day2.py
@@ -0,0 +1,25 @@
+# slicing of sting
+
+str = "devansh jindal"
+
+pr = str [2:10]
+print(pr)
+
+
+# concept of negative indexing in strings
+
+str= "devansh jindal"
+
+pr = str [-10: -1]
+
+print(pr)
+
+# to check if a string ends with perticular substring
+
+str = "hi i am devansh jindal"
+
+print(str.endswith("hi"))
+
+# capitalise the first character of the string
+
+print(str.capitalize())
